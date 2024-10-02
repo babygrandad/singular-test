d79bdc6bab6316e44a694060e11734a8048b0769
babygrandad <relebogilenkotswe@gmail.com>
2024-10-02 05:04:06 +0200

    fetching sale and product info now need to be used

--- src/Pages/SaleHistory/Salehistory.js
+++ src/Pages/SaleHistory/Salehistory.js
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { GetRecords } from '../../utils/Api/SaleHistoryApi';
+import { GetProducts } from '../../utils/Api/ProductsApi';
 
 function Salehistory() {
 	const { id } = useParams();
 	const [product, setProduct] = useState([]);
 	const [saleRecords, setSaleRecords] = useState([]);
 
+	useEffect(() => {
+		const fetchProduct = async (id) => {
+			const products = await GetProducts();
+			setProduct(products.data.find(item => item.id === id));
+		}
 
-	const fetchProduct = async (id) => {
-		
-	}
+		const fetchRecords = async (id) => {
+			setSaleRecords(await GetRecords(id));
+		}
 
-	const fetchRecords = async (id) => {
-		// get product records by the product id
-	}
+		fetchProduct(id);
+		fetchRecords(id);
+	})
 
-
-  return (
-	<div>Salehistory</div>
-  )
+	return (
+		<div>Salehistory</div>
+	)
 }
 
 export default Salehistory
